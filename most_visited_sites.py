bfaa821e1b79ca05e6d4a9a15fa48806018c1ccd
Nooralwachi <noor_alwachi@yahoo.com>
2021-02-18 19:52:41 -0800

    Create most_visited_sites.py

---
+++ most_visited_sites.py
@@ -0,0 +1,21 @@
+def most_visited_sites():
+	result = {}
+	with open("input.txt", "r") as f:
+		#file_lines = f.readlines()
+		#file_lines = [x.split() for x in file_lines]
+		for line in f:
+			line=line.split()
+			for l in line[1:]:
+				if l in result:
+					result[l] += 1
+				else:
+					result[l] = 1
+	sorted_result = sorted(result.items(), key = lambda kv:(kv[1], kv[0]), reverse=True)
+	if len(sorted_result) <= 5:
+		print(sorted_result)
+	else:
+		for x,y in sorted_result[:5]:
+			print(f'{x} {y}')
+
+
+most_visited_sites()
